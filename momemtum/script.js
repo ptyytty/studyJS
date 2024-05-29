1309ee3fec7023dbb3b8edb4fb5f2853219f8622
ptyytty <typark@gmail.com>
2024-05-29 14:01:27 +0900

    study JavaScript

---
+++ momemtum/script.js
@@ -0,0 +1,67 @@
+// const a = 5;
+// const b = 2;
+// const myName = "typark";
+
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log("hello " + myName);
+
+// const amIFat = null;
+// let something; // = undefined 값이 없음
+
+// console.log(amIFat);
+
+// const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// console.log(daysOfWeek);
+
+// daysOfWeek.push("sun");
+
+// console.log(daysOfWeek);
+
+// const player = {
+//     name: "typark",
+//     points: 10,
+//     fat: false,
+// }
+
+// console.log(player);
+// console.log(player.name);
+
+// player.lastname = "potato";
+// console.log(player);
+
+// function sayHello(nameOfPerson, age){
+//     console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+
+// }
+
+// sayHello("park", 24);
+// sayHello("lee", 21);
+// sayHello("kim", 30);
+
+// const player = {
+//     name: "typark",
+//     sayHello: function(otherPersonsName){
+//         console.log("Hello " + otherPersonsName);
+//     }
+// }
+
+// console.log(player.name);
+// player.sayHello("lynn");
+
+// const age = parseInt(prompt("How old are you?"));
+// console.log(age);
+
+// const age = parseInt(prompt("How old are you?"));
+// if(isNaN(age)){
+//     console.log("Please write a number");
+// }else if(age<18){
+//     console.log("You are too young");
+// }else{
+//     console.log("You can drink");
+// }
+
+const title = document.getElementById("title");
+title.innerText = "Got you!";
