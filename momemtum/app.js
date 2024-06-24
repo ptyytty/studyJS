a8fea9a33fee8e8616a67dac0c9a6ad602d4f21c
ptyytty <typark@gmail.com>
2024-06-25 03:54:50 +0900

    Dev Momentum

---
+++ momemtum/app.js
@@ -0,0 +1,14 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+function onLoginBtnClick(){
+    const username = loginInput.value;
+    if(username === ""){
+        alert("Please write your name");
+    }else if(username.length > 15){
+        alert("Your name is too long");
+    }
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
